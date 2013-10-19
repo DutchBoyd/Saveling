7f9261075b35207e9eb7861c89c7deb48df8b88b
Robert Boyd <robert.boyd@me.com>
2013-10-19 16:37:38 -0500

    Adding Mandrill for transactional email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ SavelingWeb::Application.configure do
 
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
-      :port      => 25,
+      :port      => 587,
       :user_name => ENV["MANDRILL_USERNAME"],
       :password  => ENV["MANDRILL_API_KEY"]
     }
@@ -86,4 +86,6 @@ SavelingWeb::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.mandrill_mailer.default_url_options = { :host => 'saveling.com' }
 end
