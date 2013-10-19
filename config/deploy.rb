ce27a62f4fce27c74ded828e91ed2c4c4c69e1f5
Robert Boyd <robert.boyd@me.com>
2013-10-19 02:14:42 -0500

    Setting deployment RAILS_ENV=production.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :scm, :git
 set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :rails_env, 'production'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
