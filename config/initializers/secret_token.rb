e873f4d8c62a8d2671eeedee36237ca704beb143
Joshua Smith <jds5x4@gmail.com>
2013-10-18 19:48:02 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SavelingWeb::Application.config.secret_key_base = '079089f26074f382e151a4876ab98dd0e3e977bba9df2d21dce20a9ce26b41cd68a74919a436dc3252bd4551eb680906d488b8d79bf0b8ff5fcffcd60b5a6b09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SavelingWeb::Application.config.session_store :cookie_store, key: '_saveling-web_session'
